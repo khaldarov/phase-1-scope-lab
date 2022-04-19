749d5904c04658b13dd68502dfde8170b886de60
khaldar <adnan-5-@outlook.com>
2022-04-19 20:46:05 +0300

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "absolutely not bob";
+console.log(customerName);
+//
+function upperCaseCustomerName() {
+    customerName = "BOB";
+    return customerName;
+}
+//
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+//
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+//
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "michael g. scott";
+    return leastFavoriteCustomer;
+}
+// console.log(changeLeastFavoriteCustomer())
